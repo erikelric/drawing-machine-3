7174ee704a83546332b520a6ec0c559f1d8f1a49
erikelric <60452164+erikelric@users.noreply.github.com>
2020-10-23 22:59:26 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,18 +2,21 @@ let array = [];
 let noiseOffset = 0.0;
 let strokeWidth = 5;
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220, 50, 133);
 
 
-
+  drawGrid();
+  strokeWeight(5);
+  noFill();
 
 }
 
 function draw() {
 
-  background(220, 50, 133, 5);
+  // background(220, 50, 133, 5);
   strokeWeight(strokeWidth);
 
   noiseOffset =+ 0.05;
@@ -42,4 +45,28 @@ function keyTyped(){
     }
 
     return false;
+}
+
+// function mouePressed() {
+//   array = [];
+//   backgroundColor = 255;
+// }
+
+function drawGrid() {
+  numCells = 10;
+  fillColor = 250;
+strokeWeight(0);
+
+  for (let i = 0; i <= width; i += width / numCells){
+    for (let j = 0; j <= height; j += height / numCells){
+      if (fillColor === 255){
+        fillColor = 150;
+      } else {
+        fillColor = 255;
+      }
+      fill(fillColor);
+      rect(i, j, width / numCells, height / numCells);
+    }
+  }
+  strokeWeight(5);
 }
